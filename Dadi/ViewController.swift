92bece5de9da2ce55da7f87329d355ca83c79819
Dídac Serrano <didac.serrano@badiapp.com>
2020-08-20 16:56:59 +0200

    Initial Commit

---
+++ Dadi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dadi
+//
+//  Created by Dídac Serrano i Segarra on 20/08/2020.
+//  Copyright © 2020 Dídac Serrano i Segarra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
